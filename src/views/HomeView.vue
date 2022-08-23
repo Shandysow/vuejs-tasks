0eb34d18d97b944700fa2c4a27041c40cbc9f416
Shandysow <sow23senthil@gmail.com>
2022-08-23 19:38:04 +0530

    tasks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="pink lighten-5">
+<form>
 <h1><center>FORM REGISTRATION</center></h1>
 <br>
       <v-row>
@@ -62,13 +63,13 @@
 <label for="gender"><b><i>GENDER :</i></b></label>
 <div class="space">
   </div>
-<input type="radio" id="male" name="gender" value="MALE">
+<input type="radio" id="male" v-model="gender" value="MALE">
 <div class="space1">
   </div>
 <label for="male">MALE</label>
 <div class="space2">
   </div>
-<input type="radio" id="female" name="gender" value="FEMALE">
+<input type="radio" id="female" v-model="gender" value="FEMALE">
 <div class="space3">
   </div>
 <label for="female">FEMALE</label><br>
@@ -78,19 +79,19 @@
   <label for="interest"><b><i>INTEREST :</i></b> </label>
 <div class="space4">
   </div>
-<input type="checkbox" id="interest1" name="interest1" value="CRICKET">
+<input type="checkbox" id="interest1" v-model="interest" value="CRICKET">
 <div class="space5">
   </div>
 <label for="interest1">CRICKET</label>
 <div class="space6">
   </div>
-<input type="checkbox" id="interest2" name="interest2" value="HOCKEY">
+<input type="checkbox" id="interest2" v-model="interest" value="HOCKEY">
 <div class="space7">
   </div>
 <label for="interest2">HOCKEY</label>
 <div class="space8">
   </div>
-<input type="checkbox" id="interest3" name="interest3" value="FOOTBALL">
+<input type="checkbox" id="interest3" v-model="interest" value="FOOTBALL">
 <div class="space9">
   </div>
 <label for="interest3">FOOTBALL</label>
@@ -100,7 +101,7 @@
 <v-row>
       <v-col cols="4">
         <v-combobox
-          v-model="select"
+          v-model="location"
           :items="items"
           multiple
           outlined
@@ -114,13 +115,70 @@
     align="center"
     justify="space-around"
   >
-    <v-btn depressed color ="primary">
+    <v-btn depressed color ="primary" v-on:click="onsubmit()">
       SUBMIT
     </v-btn>
- <v-btn depressed color ="error" v-on:click="resetInput()">
+ <v-btn depressed color ="normal" v-on:click="resetInput()">
       CANCEL
     </v-btn>
     </v-row>
+</form>
+<br>
+ <v-simple-table dark>
+    <template v-slot:default>
+      <thead>
+        <tr>
+         <th class="text-left">
+           ID
+          </th>
+          <th class="text-left">
+            NAME
+          </th>
+          <th class="text-left">
+            EMAIL ID
+          </th>
+            <th class="text-left">
+            GENDER
+          </th>
+          <th class="text-left">
+            INTEREST
+          </th>
+           <th class="text-left">
+            LOCATION
+          </th>
+
+          <th class="text-left">
+          UPDATE ACTION
+             </th>
+             
+ <th class="text-left">
+          DELETE ACTION
+             </th> 
+   </tr>
+      </thead>
+     
+      <tbody>
+        <tr v-for="(item, index) in all" :key="index">
+         <th scope="row">{{ index+1 }}</th>
+          <td>{{ item.name }}</td>
+          <td>{{ item.email }}</td>
+           <td>{{ item.gender }}</td>
+          <td>{{ item.interest }}</td>
+           <td>{{ item.location }}</td>
+           <td> <v-btn depressed color ="teal" @click="Update()">
+               UPDATE
+            </v-btn></td>
+            <td> <v-btn depressed color ="error" @click="deleteTableRow(index)">
+               DELETE
+            </v-btn></td>
+
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+
+
+
     <HelloWorld />
     </div>
 </template>
@@ -136,11 +194,15 @@
       return {
         name: '',
         email: '',
-        pass: '',
-        password: '',
+        pass:'',
+        password:'',
+        interest:[],
+        gender:'',
+        location:[],
+        all: [],
         rules: {
           req: [val => (val || '').length > 0 || 'Mandatory Field', val => (!val) || /^[a-zA-Z\s]*$/.test(val) || 'Please type only alphabets'],
-           required: value => !!value || 'Required.',
+          required: value => !!value || 'Required.',
           email: value => {
             const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
             return pattern.test(value) || 'Invalid e-mail. Check again'
@@ -193,6 +255,19 @@
       }
       this.password = generate(this.pass)
     },
+
+   
+
+onsubmit()
+{
+   this.all.push({ name: this.name, email: this.email, gender: this.gender, interest: this.interest, location: this.location });
+   console.log(this.all);
+},
+deleteTableRow(index) { 
+  console.log(index)
+      this.all.splice(index, 1);      
+   },
+
  resetInput() {
       this.name = "";
       this.email = "";
